2be229cc78eb3e660fa374c605bbe453723f0413
Jared Langson <langson.jared@gmail.com>
2021-11-30 08:45:24 -0500

    Change make-input to create a hashmap of irgb
    i is index.
    rgb red green blue.

--- src/audio_reactive_led_strip_clojure/simulated.clj
+++ src/audio_reactive_led_strip_clojure/simulated.clj
@@ -1,18 +1,34 @@
 (ns audio-reactive-led-strip-clojure.simulated)
 
-(defn- input []
-  (int (Math/floor (rand 256))))                ;TODO should this be 256?
+(defn- random []
+  (int (Math/floor (rand 256))))                ;should this be 256?
 
-(defn- input->output [input])
+;The packet encoding scheme is:
+;|i|r|g|b|
+;where
+;i (0 to 255): Index of LED to change (zero-based)
+;r (0 to 255): Red value of LED
+;g (0 to 255): Green value of LED
+;b (0 to 255): Blue value of LED
 
-(defn- output->led [output])
+(defn- make-input []
+  {:i (random)
+   :r (random)
+   :g (random)
+   :b (random)})
+
+;;does graphics
+;;using clojure2d  https://github.com/Clojure2D/clojure2d
+(defn- input->led
+  [input])
 
 (defn run-simulated [input]
-  (-> input
-      input->output
-      output->led))
+  (input->led input))
+
+
+
 
 (comment
-  ;todo loop this
-  (run-simulated input)
- )
+  (doseq [i (range 10)]
+    (run-simulated i))
+  )
